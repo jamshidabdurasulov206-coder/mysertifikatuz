770ed0303ce25d00d90ba08732cc243e25e1c987
Javlonbek Abdurasulov <jabdurasulov@amcbridge.com>
2026-04-02 23:59:43 +0500

    deployment changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL
+  || (typeof window !== "undefined" && window.location?.origin
+    ? `${window.location.origin}/api`
+    : "/api");
+
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
